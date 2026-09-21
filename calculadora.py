b00b319f3d4c3c3ae77363ce06e235eeed591dd1
vela <fvela2006@gmail.com>
2026-09-21 09:17:05 +0200

    Primera version: calculadora dividida en modulos y paquete

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+from mi_paquete import operaciones
+from mi_paquete import utils
+
+utils.mostrar_mensaje()
+print("5 + 3 =", operaciones.sumar(5, 3))
+print("5*3=", operaciones.multiplicar(5,3))
+print("5/3=", operaciones.dividir(5,3))
